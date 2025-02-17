aa72914590f7aadfec7e782c9bddfbab5a853d29
Rungroj Wongkaeo <63869763+RungrojWK@users.noreply.github.com>
2025-02-17 14:48:16 +0700

    Add OTP service implementation with Jest configuration and environment setup

--- src/modules/otp/otp.service.ts
+++ src/modules/otp/otp.service.ts
@@ -1,27 +1,39 @@
 import {AxiosInstance} from "axios"
-import {OtpSendRequest} from "./dto/otp.dto"
+import {OtpResendRequest, OtpSendRequest, OtpSendResponse, OtpVerifyResponse, VerifyOtpRequest} from "./dto/otp.dto"
 
 export class OtpService {
   constructor(private httpClient: AxiosInstance, private secret: string) {}
 
-  public async send(data: OtpSendRequest): Promise<any> {
-    const response = await this.httpClient.post("/otp/send", data, {
+  public async send(request: OtpSendRequest): Promise<OtpSendResponse> {
+    const response = await this.httpClient.post("/otp/send", request, {
       headers: {Authorization: `Bearer ${this.secret}`}
     })
-    return response.data
+    const data = response.data?.data
+    if (!data) {
+      throw new Error("No data returned from OTP send API")
+    }
+    return data
   }
 
-  public async verify(data: any): Promise<any> {
-    const response = await this.httpClient.post("/otp/verify", data, {
+  public async verify(request: VerifyOtpRequest): Promise<OtpVerifyResponse> {
+    const response = await this.httpClient.post("/otp/verify", request, {
       headers: {Authorization: `Bearer ${this.secret}`}
     })
-    return response.data
+    const data = response.data?.data
+    if (!data) {
+      throw new Error("No data returned from OTP verify API")
+    }
+    return data
   }
 
-  public async resend(data: any): Promise<any> {
-    const response = await this.httpClient.post("/otp/resend", data, {
+  public async resend(request: OtpResendRequest): Promise<OtpSendResponse> {
+    const response = await this.httpClient.post("/otp/resend", request, {
       headers: {Authorization: `Bearer ${this.secret}`}
     })
-    return response.data
+    const data = response.data?.data
+    if (!data) {
+      throw new Error("No data returned from OTP resend API")
+    }
+    return data
   }
 }
